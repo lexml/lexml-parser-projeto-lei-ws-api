206f60d354e082416f7dc744c184a3ecaac28924
Joao Nicola <joaoraf@gmail.com>
2019-04-23 14:35:27 +0000

    Extração de pacote scalaxb para pacote externo (scalaxb-runtime), para evitar conflito com lexml-schema-scala.

--- src/main/scala/br/gov/lexml/parser/pl/ws/Dependencies.scala
+++ src/main/scala/br/gov/lexml/parser/pl/ws/Dependencies.scala
@@ -1,31 +1,28 @@
 package br.gov.lexml.parser.pl.ws
 
-import br.gov.lexml.parser.pl.ws.data.scalaxb.TipoSaida
-import br.gov.lexml.parser.pl.ws.data.scalaxb.TipoTipoDeSaida
-import br.gov.lexml.parser.pl.ws.data.scalaxb.PDF_DERIVADO
-import br.gov.lexml.parser.pl.ws.data.scalaxb.RTF_DERIVADO
-import br.gov.lexml.parser.pl.ws.data.scalaxb.EPUB_DERIVADO
-import br.gov.lexml.parser.pl.ws.data.scalaxb.ZIP_DERIVADO
-import br.gov.lexml.parser.pl.ws.data.scalaxb.XML_DERIVADO
-import br.gov.lexml.parser.pl.ws.data.scalaxb.PDF_DIFF
-import br.gov.lexml.parser.pl.ws.data.scalaxb.ParserRequisicao
-import br.gov.lexml.parser.pl.ws.data.scalaxb.EXTERNO
-import br.gov.lexml.parser.pl.ws.data.scalaxb.TipoTiposDeSaidas
+import br.gov.lexml.parser.pl.ws.data.scalaxb._
+import scalaxb._
 
 object Dependencies {
+  val tipoTipoDeSaida = { (tt : TipoSaida, tf : TipoFormatoSaida) => 
+    TipoTipoDeSaida(Map("@tipo" -> DataRecord(tt), "@formato" -> DataRecord(tf))) } 
   val depMap = Map[TipoSaida, Set[TipoSaida]](
     EPUB_DERIVADO -> Set(XML_DERIVADO),
     ZIP_DERIVADO -> Set(XML_DERIVADO),
     PDF_DERIVADO -> Set(XML_DERIVADO),
     PDF_DIFF -> Set(RTF_DERIVADO)).withDefaultValue(Set[TipoSaida]())
-  def deps(t: TipoTipoDeSaida): Seq[TipoTipoDeSaida] = t +: depMap(t.tipo).to[Seq].map(tt => TipoTipoDeSaida(tt, EXTERNO))
+  def deps(t: TipoTipoDeSaida): Seq[TipoTipoDeSaida] = t +: depMap(t.tipo).to[Seq].map(tipoTipoDeSaida(_,EXTERNO))
 
   def completeDependencies(req: ParserRequisicao): ParserRequisicao = {
     val alwaysParse = req.opcoes.flatMap(_.alwaysParse).getOrElse(true)
     val saidas = req.saidas.tipoSaida
-    val saidas1 = if (alwaysParse && !saidas.contains(XML_DERIVADO)) { saidas :+ TipoTipoDeSaida(XML_DERIVADO, EXTERNO) } else { saidas }
+    val saidas1 = if (alwaysParse && !saidas.contains(XML_DERIVADO)) { 
+        saidas :+ TipoTipoDeSaida(Map(
+            "@tipo" -> DataRecord(XML_DERIVADO : TipoSaida), 
+            "@formato" -> DataRecord(EXTERNO : TipoFormatoSaida))) 
+       } else { saidas }
     val saidas2 = saidas1.flatMap(deps) ++ saidas1
-    val saidas3 = saidas2.map(t => (t.tipo, t.formato)).toMap.map { case (t, f) => TipoTipoDeSaida(t, f) }.toSeq
-    req.copy(saidas = TipoTiposDeSaidas(saidas3: _*))
+    val saidas3 = saidas2.map(t => (t.tipo, t.formato)).toMap.map(tipoTipoDeSaida.tupled).to[Seq]
+    req.copy(saidas = TipoTiposDeSaidas(saidas3))
   }
 }
