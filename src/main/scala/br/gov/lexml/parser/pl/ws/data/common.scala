eec748a5b0cd8af1bad032a3808ac732a5eecd90
joaoraf <joaoraf@gmail.com>
2020-11-17 11:14:55 -0300

    Correcao de typo em uri de namespace

--- src/main/scala/br/gov/lexml/parser/pl/ws/data/common.scala
+++ src/main/scala/br/gov/lexml/parser/pl/ws/data/common.scala
@@ -3,5 +3,5 @@ package br.gov.lexml.parser.pl.ws.data
 object XMLConsts {
   val rsNamespace = "http://www.lexml.gov.br/schema/remissoes"
   val xlinkNamespace = "http://www.w3.org/1999/xlink"
-  val prNamespace = "http://www.lexml.gov.br/parser-ws:w"
+  val prNamespace = "http://www.lexml.gov.br/parser-ws"
 }
