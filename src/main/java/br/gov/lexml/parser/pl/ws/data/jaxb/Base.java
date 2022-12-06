61af54604083f87af76dc9983b64214a8258650e
joaoraf <joaoraf@gmail.com>
2022-12-06 18:07:13 -0300

    skip javadoc generation

--- src/main/java/br/gov/lexml/parser/pl/ws/data/jaxb/Base.java
+++ src/main/java/br/gov/lexml/parser/pl/ws/data/jaxb/Base.java
@@ -3,17 +3,26 @@ package br.gov.lexml.parser.pl.ws.data.jaxb;
 import org.pojomatic.Pojomatic;
 import org.pojomatic.annotations.AutoProperty;
 
+/**
+ * <p>Abstract Base class.</p>
+ *
+ * @author joao
+ * @version $Id: $Id
+ */
 @AutoProperty
 @jakarta.xml.bind.annotation.XmlTransient
 public abstract class Base {
+	  /** {@inheritDoc} */
 	  @Override final public boolean equals(Object o) {
 	    return Pojomatic.equals(this, o);
 	  }
 
+	  /** {@inheritDoc} */
 	  @Override final public int hashCode() {
 	    return Pojomatic.hashCode(this);
 	  }
 
+	  /** {@inheritDoc} */
 	  @Override final public String toString() {
 	    return Pojomatic.toString(this);
 	  }
